0c6f3c479e80dc0c3c39ff80d2401d011459f9e5
Sivabala-Manikandan <sivabalamanikandan03@gmail.com>
2026-04-20 23:34:19 +0530

    Files updated

--- server/index.js
+++ server/index.js
@@ -35,8 +35,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// Pre-flight support for all routes
-// app.options('*', cors(corsOptions)); // Removed to avoid path-to-regexp error
+
 
 app.use(express.json());
 
